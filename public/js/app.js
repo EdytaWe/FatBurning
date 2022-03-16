dbffeb3653c6227e92dd8a5077037a7b9bed12ed
EdytaWE <edytawesolowska@gmail.com>
2022-03-16 01:58:35 +0100

    add hamburger menu styles and js script

---
+++ public/js/app.js
@@ -0,0 +1,21 @@
+const hamburger = document.querySelector("#hamburger");
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menu__item");
+
+const showMenu = () =>{
+    menu.classList.toggle("menu__mobile");
+    menuItems.forEach(el=>{
+        el.style.width = "100vw";
+        el.style.padding = "15px";
+        el.style.fontSize = "1.25rem";
+    });
+    hamburger.classList.toggle("fa-bars");
+    hamburger.classList.toggle("fa-xmark");
+    if(hamburger.classList.contains("fa-xmark")){
+        hamburger.style.transform = "translatex(50px)";
+    }else{
+        hamburger.style.transform = "translatex(0px)";
+    }
+}
+
+hamburger.addEventListener("click", showMenu);
